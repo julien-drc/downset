5b9ad0bb1f695c9e77201fd891654ed8b56fac47
Dumont Julien <lapz95@hotmail.fr>
2022-01-27 17:53:53 +0100

    follow unfollow

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -51,3 +51,57 @@ module.exports.deleteUser = (req, res) => {
     return res.status(502).send({ message: err})
   }
 }
+
+module.exports.follow = (req, res) => {
+  if(!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToFollow))
+    return res.status(400).send('ID unknow : ' + req.params.id)
+
+  try {
+    UserModel.findByIdAndUpdate(
+      req.params.id,
+      { $addToSet: { following: req.body.idToFollow}},
+      { new: true, upsert: true },
+      (err, docs) => {
+        if(!err) res.status(201).json(docs)
+        else return res.status(400).json(err)
+      }
+    )
+    UserModel.findByIdAndUpdate(
+      req.body.idToFollow,
+      { $addToSet: { followers: req.params.id}},
+      { new: true, upsert: true },
+      (err, docs) => {
+        if(err) return res.status(400).json(err)
+      }
+    )
+  } catch (err) {
+    return res.status(502).send({ message: err})
+  }
+}
+
+module.exports.unfollow = (req, res) => {
+  if(!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToUnfollow))
+    return res.status(400).send('ID unknow : ' + req.params.id)
+
+  try {
+    UserModel.findByIdAndUpdate(
+      req.params.id,
+      { $pull: { following: req.body.idToUnfollow}},
+      { new: true, upsert: true },
+      (err, docs) => {
+        if(!err) res.status(201).json(docs)
+        else return res.status(400).json(err)
+      }
+    )
+    UserModel.findByIdAndUpdate(
+      req.body.idToUnfollow,
+      { $pull: { followers: req.params.id}},
+      { new: true, upsert: true },
+      (err, docs) => {
+        if(err) return res.status(400).json(err)
+      }
+    )
+  } catch (err) {
+    return res.status(502).send({ message: err})
+  }
+}
