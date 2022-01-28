25bb95c13f6ab9934fb7f1015927be383fcf2200
Dumont Julien <lapz95@hotmail.fr>
2022-01-28 01:04:40 +0100

    login path

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,6 +5,8 @@ const userController = require('../controllers/user.controller')
 
 //auth
 router.post("/register", authController.signUp)
+router.post("/login", authController.signIn)
+router.post("/logout", authController.logout)
 
 //user display
 router.get('/', userController.getAllUsers)
