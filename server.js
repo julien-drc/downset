88b92d3342c103dcbe70b895bdcd374d096de754
Dumont Julien <lapz95@hotmail.fr>
2022-01-28 17:58:11 +0100

    add post model and make CRUD on him

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/user.routes')
+const postRoutes = require('./routes/post.routes')
 require('dotenv').config({path: './config/.env'})
 require('./config/db')
 const {checkUser, requireAuth} = require('./middleware/auth.middleware')
@@ -18,6 +19,7 @@ app.get('/jwtid', requireAuth, (req, res) => {
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/post', postRoutes)
 
 
 app.listen(process.env.PORT, () => {
